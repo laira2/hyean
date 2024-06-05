ed34f49f6873b47ddbfd16217f479c868fafc965
JeongChangYeol <devcy1029@gmail.com>
2024-06-05 14:17:28 +0900

    hyean/setting.py, hyean/urls.py, openapi/views.py, templates/openapi.html í현재까업한 내용 업로드(작품명, 작품가격, 파일명(작가), 파일URL)

--- hyean/settings.py
+++ hyean/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account'
-    'openapi.apps.OpenapiConfig',
+    'account',
+    'openapi',
 ]
 
 MIDDLEWARE = [

d2fd1d8be428598f46d9961dad07c8a0903dfc73
laira <laira241@gmail.com>
2024-06-05 11:22:24 +0900

    구글인증 기본 세팅

--- hyean/urls.py
+++ hyean/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import account.views as account_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', account_view.register,name='register'),
-    path('login/', account_view.user_login, name='user_login')
+    path('login/', account_view.user_login, name='user_login'),
+    path('social-auth/', include('social_django.urls', namespace='social')),
 ]

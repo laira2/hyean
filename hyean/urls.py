9c4e0220029a1375a74c93f0c2dc429dae105a87
aiden_Lee <isyouh77@gmail.com>
2024-06-05 19:05:44 +0900

    변경사항없음

--- hyean/urls.py
+++ hyean/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger-ui/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-ui'),  # 새로운 URL 패턴 추가
 ]

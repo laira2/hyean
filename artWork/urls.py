47587a94d6b6340cc38acff448a754e8db008468
haneshi <wackymob98@naver.com>
2024-06-12 03:03:47 +0900

    검색기능 2번째 방법 실패

--- artWork/urls.py
+++ artWork/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),  # 예를 들어, index view로 연결
     path('', views.openapi_view, name='index'),
-    path('search/<str:query>/', views.search, name='search'),
+    path('search/<str:artNm>/', views.search, name='search'),
 ]
--- artWork/views.py
+++ artWork/views.py
@@ -1,45 +1,48 @@
 # artWork/views.py
 import requests.exceptions
+import requests
 from django.shortcuts import render
-import random #난수 생성 함수 모듈
-import asyncio #비동기 작업을 위한 asyncio 모듈
-import aiohttp #비동기 HTTP 클라이언트 라이브러리인 aiohttp를 가져오며, 비동기적으로 HTTP 요청 및 응답을 받아올 수 있다.
-from django.shortcuts import render #장고에서 HTML 템프릿을 랜더링하기 위한 render함수 가져옴
-from urllib.parse import urlencode #딕셔너리를 쿼리 문자열로 변환하는데 사용
+import random  #난수 생성 함수 모듈
+import asyncio  #비동기 작업을 위한 asyncio 모듈
+import aiohttp  #비동기 HTTP 클라이언트 라이브러리인 aiohttp를 가져오며, 비동기적으로 HTTP 요청 및 응답을 받아올 수 있다.
+from django.shortcuts import render  #장고에서 HTML 템프릿을 랜더링하기 위한 render함수 가져옴
+from urllib.parse import urlencode  #딕셔너리를 쿼리 문자열로 변환하는데 사용
 from haystack.query import SearchQuerySet
 
 # def index(request):
 #     return render(request, 'index.html')
 
 # 이미지 및 작품 데이터 캐싱을 위한 딕셔너리
-cached_data = { #이미지와 작품 데이터를 캐싱하기 위해 딕셔너리 초기화
-    'images': {}, #이미지 데이터 저장을 위해 사용되는 빈 딕셔너리
-    'art_names': set(), #작품명을 저장하는데 사용되는 빈 집합(set)
-    'art_dimensions': {}, #작품의 가로 세로 정보를 저장할 딕셔너리 추가
-    'art_info': {} #작품 id, 카테고리 정보를 저장할 딕셔너리 추가
+cached_data = {  #이미지와 작품 데이터를 캐싱하기 위해 딕셔너리 초기화
+    'images': {},  #이미지 데이터 저장을 위해 사용되는 빈 딕셔너리
+    'art_names': set(),  #작품명을 저장하는데 사용되는 빈 집합(set)
+    'art_dimensions': {},  #작품의 가로 세로 정보를 저장할 딕셔너리 추가
+    'art_info': {}  #작품 id, 카테고리 정보를 저장할 딕셔너리 추가
 }
 
+
 async def fetch(session, url, cache_key=None):
-#비동기적으로 데이터를 가져오고 캐싱처리 진행
-#cache_key=None는 캐시 키로, 기본값이 None이며, 해당 키를 사용하여 데이터 캐시 진행
-    global cached_data # 함수 내에서 cached_data를 수정하기 위해 전역변수 선언
+    #비동기적으로 데이터를 가져오고 캐싱처리 진행
+    #cache_key=None는 캐시 키로, 기본값이 None이며, 해당 키를 사용하여 데이터 캐시 진행
+    global cached_data  # 함수 내에서 cached_data를 수정하기 위해 전역변수 선언
 
-    if cache_key and cache_key in cached_data['images']: #cache_key가 주어졌고, 딕셔너리에 있다면
-        return cached_data['images'][cache_key] #캐시된 데이터 반환
+    if cache_key and cache_key in cached_data['images']:  #cache_key가 주어졌고, 딕셔너리에 있다면
+        return cached_data['images'][cache_key]  #캐시된 데이터 반환
 
-    async with session.get(url) as response: #비동기 실행 후, 응답을 response 변수에 저장 후 응답대기
-        data = await response.json() #응답객체에서 json 데이터를 비동기적으로 추출 후 data 저장
+    async with session.get(url) as response:  #비동기 실행 후, 응답을 response 변수에 저장 후 응답대기
+        data = await response.json()  #응답객체에서 json 데이터를 비동기적으로 추출 후 data 저장
 
-        if cache_key: #cache_key가 있는 경우 이미지값을 딕셔너리에 저장
+        if cache_key:  #cache_key가 있는 경우 이미지값을 딕셔너리에 저장
             cached_data['images'][cache_key] = data
 
-        return data #캐시키와 이미지값을 호출 시 반환
+        return data  #캐시키와 이미지값을 호출 시 반환
+
 
 async def get_data(base_url):
     async with aiohttp.ClientSession() as session:
-    #aiohttp의 Clientsession 객체를 사용, 비동기적으로 해당 URL에 GET요청
-    #session은 HTTP 요청을 보내는데 사용
-        tasks = [] #비동기 작업들을 저장할 빈 리스트 생성
+        #aiohttp의 Clientsession 객체를 사용, 비동기적으로 해당 URL에 GET요청
+        #session은 HTTP 요청을 보내는데 사용
+        tasks = []  #비동기 작업들을 저장할 빈 리스트 생성
         for page_number in range(0, 3):
             params = {
                 "serviceKey": "gKat/nvnmi8i9zoiX+JsGzCTsAV75gkvU71APhj8FbnH3yX4kiZMuseZunM0ZpcvKZaMD0XsmeBHW8dVj8HQxg==",
@@ -56,30 +59,31 @@ async def get_data(base_url):
         responses = await asyncio.gather(*tasks)
         #tasks리스트에 있는 모든 비동기 작업을 동시에 실행
 
-    # 작품명 및 이미지 데이터 캐싱
-        for response in responses: #비동기적으로 실행한 HTTP 요청 결과를 담고 있는 리스트로, 반복문 사용
+        # 작품명 및 이미지 데이터 캐싱
+        for response in responses:  #비동기적으로 실행한 HTTP 요청 결과를 담고 있는 리스트로, 반복문 사용
             items = response.get('response', {}).get('body', {}).get('items', [])
             #각 응답에서 작품 정보를 추출하며, 위 내용중 하나라도 존재하지 않으면 빈 리스트 반환
-            if items: #응답에서 추출한 작품 정보가 존재하는지 확인
-                for item in items: #작품정보가 있을 경우 각 작품 정보에 대해 반복
+            if items:  #응답에서 추출한 작품 정보가 존재하는지 확인
+                for item in items:  #작품정보가 있을 경우 각 작품 정보에 대해 반복
                     art_name = item.get('artNm')
                     if art_name:
-                        art_name_stripped = art_name.strip() #작품명 있을경우 양쪽 공배 제거 후 변수에 할당
+                        art_name_stripped = art_name.strip()  #작품명 있을경우 양쪽 공배 제거 후 변수에 할당
                         if art_name_stripped:
                             cached_data['art_names'].add(art_name_stripped)
                             #캐싱된 데이터에 작품명 추가. 중복된 작품명은 자동 제거
-                            art_width = item.get('artWidth') #작품 가로
-                            art_vrticl = item.get('artVrticl') #작품 세로
-                            artCd = item.get('artCd') #작품 id
-                            categry = item.get('categry') if item.get('categry') else '기타 '#작품 카테고리, 없는 경우 기타 입력
-                            cached_data['art_dimensions'][art_name_stripped] = { #가로, 세로 값 딕셔너리 저장
+                            art_width = item.get('artWidth')  #작품 가로
+                            art_vrticl = item.get('artVrticl')  #작품 세로
+                            artCd = item.get('artCd')  #작품 id
+                            categry = item.get('categry') if item.get('categry') else '기타 '  #작품 카테고리, 없는 경우 기타 입력
+                            cached_data['art_dimensions'][art_name_stripped] = {  #가로, 세로 값 딕셔너리 저장
                                 'art_width': art_width,
                                 'art_vrticl': art_vrticl
                             }
-                            cached_data['art_info'][art_name_stripped] = { #작품 일련번호, 카테고리 값 딕셔너리 저장
+                            cached_data['art_info'][art_name_stripped] = {  #작품 일련번호, 카테고리 값 딕셔너리 저장
                                 'artCd': artCd,
                                 'categry': categry
                             }
+
 
 async def openapi_view(request):
     base_url = "http://apis.data.go.kr/5710000/benlService/nltyArtList"
@@ -96,17 +100,17 @@ async def openapi_view(request):
     #이미지 정보를 저장할 빈 딕셔너리 생성
 
     async with aiohttp.ClientSession() as session:
-    #비동기 HTTP 요청을 수행하기 위해 aiohttp 모듈 사용하여 클라이언트 세션 생성
-    #해당 세션은 HTTP 요청을 보내기 위한 컨텍스트 매니저로 사용
-        for art_name in info_list: #이미지를 가져올 작품명을 info_list에서 작품명을 반복하여 가져옴
-            image_params = { #이미지를 가져오기 위해 파라미터 설정
+        #비동기 HTTP 요청을 수행하기 위해 aiohttp 모듈 사용하여 클라이언트 세션 생성
+        #해당 세션은 HTTP 요청을 보내기 위한 컨텍스트 매니저로 사용
+        for art_name in info_list:  #이미지를 가져올 작품명을 info_list에서 작품명을 반복하여 가져옴
+            image_params = {  #이미지를 가져오기 위해 파라미터 설정
                 "serviceKey": "gKat/nvnmi8i9zoiX+JsGzCTsAV75gkvU71APhj8FbnH3yX4kiZMuseZunM0ZpcvKZaMD0XsmeBHW8dVj8HQxg==",
                 "pageNo": "1",
                 "numOfRows": "5",
                 "returnType": "json",
                 "artNm": art_name
             }
-            try: #비동기적으로 이미지를 가져오는 작업 수행
+            try:  #비동기적으로 이미지를 가져오는 작업 수행
                 full_url = image_api_url + '?' + urlencode(image_params)
                 #이미지 API의 전체 URL 생성
                 image_response = await fetch(session, full_url, cache_key=full_url)
@@ -121,8 +125,8 @@ async def openapi_view(request):
                             file_name = image_item.get('fileNm', '')
                             file_url = image_item.get('fileUrl', '')
                             if file_name and file_url:
-                                file_name_prefix = file_name[:4] #파일 이름 4글자 추출
-                                image_info_dict[file_name_prefix] = { #이미지 정보 딕셔너리에 저장
+                                file_name_prefix = file_name[:4]  #파일 이름 4글자 추출
+                                image_info_dict[file_name_prefix] = {  #이미지 정보 딕셔너리에 저장
                                     'art_name': art_name,
                                     'file_name': file_name,
                                     'file_url': file_url,
@@ -131,70 +135,65 @@ async def openapi_view(request):
                                     'artCd': cached_data['art_info'].get(art_name, {}).get('artCd', ''),
                                     'categry': cached_data['art_info'].get(art_name, {}).get('categry', '')
                                 }
-                else:
-                    print(f"이미지를 가져오지 못했습니다. {art_name}.") #이미지 데이터가 없는경우 작품명 출력
+                    else:
+                        print(f"이미지를 가져오지 못했습니다. {art_name}.")  #이미지 데이터가 없는경우 작품명 출력
             except aiohttp.ClientError as e:
-                print(f"이미지를 가져오는 동안 오류가 발생했습니다. {art_name}: {e}") #오류발생 시 세부정보와 작품명 출력
-                print("3초 후 다시 시도합니다.") #네트워크 등의 오류 발생 시 3초 대기 문구 출력
-                await asyncio.sleep(3) #3초 대기, 비동기적으로 일시 중단 후 지정 시간 후 코드 실행
+                print(f"이미지를 가져오는 동안 오류가 발생했습니다. {art_name}: {e}")  #오류발생 시 세부정보와 작품명 출력
+                print("3초 후 다시 시도합니다.")  #네트워크 등의 오류 발생 시 3초 대기 문구 출력
+                await asyncio.sleep(3)  #3초 대기, 비동기적으로 일시 중단 후 지정 시간 후 코드 실행
 
-    for image_info in image_info_dict.values(): #작품가격을 위한 랜덤함수 사용
-        price = random.randint(1000, 10000) * 10000 #천에서 만사이의 랜덤 정수 선택 후 만 곱하기
+    for image_info in image_info_dict.values():  #작품가격을 위한 랜덤함수 사용
+        price = random.randint(1000, 10000) * 10000  #천에서 만사이의 랜덤 정수 선택 후 만 곱하기
         image_info['price'] = price
 
-    image_info_list = list(image_info_dict.values()) #매개변수의 값을 리스트 형태로 반환하여 저장
+    image_info_list = list(image_info_dict.values())  #매개변수의 값을 리스트 형태로 반환하여 저장
     return render(request, 'index.html', {'image_info_list': image_info_list})
+
+
 
 def search(request):
     base_url = "http://apis.data.go.kr/5710000/benlService/nltyArtList"
     image_api_url = "http://apis.data.go.kr/5710000/benlService/artImgList"
 
-    image_params = {  # 이미지를 가져오기 위해 파라미터 설정
+    # 검색어 가져오기
+    search_query = request.GET.get('q', '')
+
+    params = {
         "serviceKey": "gKat/nvnmi8i9zoiX+JsGzCTsAV75gkvU71APhj8FbnH3yX4kiZMuseZunM0ZpcvKZaMD0XsmeBHW8dVj8HQxg==",
         "pageNo": "1",
         "numOfRows": "5",
         "returnType": "json",
-        "artNm": art_name
+        "artNm": search_query
     }
 
-    try:
-        # 작품 목록 API 요청
-        response = client.search(
-            index="{index}",
-            doc_type="{type}",
-            body={
-                "query": {
-                    "match_all": {}
-                }
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        art_list = data['response']['body']['items']['item']
+
+        # 작품 이미지 정보
+        for art in art_list:
+            image_params = {
+                "serviceKey": "gKat/nvnmi8i9zoiX+JsGzCTsAV75gkvU71APhj8FbnH3yX4kiZMuseZunM0ZpcvKZaMD0XsmeBHW8dVj8HQxg==",
+                "pageNo": "1",
+                "numOfRows": "5",
+                "returnType": "json",
+                "artNm": art["artNm"]  # 'art_name' 대신 'art["artNm"]' 사용
             }
-        )
 
-        response.raise_for_status()
-
-        # 응답 데이터 처리
-        data = response.jsoni()
-
-        # 검색결과 출력먼저
-        if data['response']['body']['items']:
-            print(f"'{keyword}'에 해당하는 작품목록:")
-            for item in data['response']['body']['items']['item']:
-                print(f"-{item['cllctNum']}")
-
-                # 이미지 api
-                image_params = {
-                    "serviceKey": "gKat/nvnmi8i9zoiX+JsGzCTsAV75gkvU71APhj8FbnH3yX4kiZMuseZunM0ZpcvKZaMD0XsmeBHW8dVj8HQxg==",
-                    "cllctnId": item["cllctnId"],
-                    "_type": "json"
-                }
-                image_reponse = requests.get(image_api_url, params=image_params)
-                image_data = image_reponse.json()
-
-                # 작품 이미지 url 출력
-                if image_data['response']['body']['items']:
-                    print(f" 이미지 URL: {image_data['response']['body']['items']['item']['imgUrl']}")
+            image_response = requests.get(image_api_url, params=image_params)
+            if image_response.status_code == 200:
+                image_data = image_response.json()
+                if 'item' in image_data['response']['body']['items']:
+                    art["image_url"] = image_data["response"]["body"]["items"]["item"]["imgUrl"]
                 else:
-                    print("이미지 정보없음")
-        else:
-            print(f"'{keyword}'에 해당하는 작품이 없습니다.")
-    except requests.exceptions.RequestException as e:
-        print(f"API 요청 중 오류 발생: {e}")
+                    art["image_url"] = None
+            else:
+                art["image_url"] = None
+    else:
+        # 에러 처리
+        print("API 요청 실패:", response.status_code)
+        art_list = []
+
+    return render(request, 'index.html', {'art_list': art_list, 'search_query': search_query})

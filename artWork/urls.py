310be2c04f1aa053499d446764291e488ca3dab4
haneshi <wackymob98@naver.com>
2024-06-11 13:50:33 +0900

    main(index.html) page upset

--- artWork/urls.py
+++ artWork/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views  # artWork의 views를 import
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # 예를 들어, index view로 연결
+    # path('', views.index, name='index'),  # 예를 들어, index view로 연결
+    path('', views.openapi_view, name='index')
 ]

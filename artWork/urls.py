61cb2fc0c3752d33e1e9362dcc62b46624118a13
aiden_Lee <isyouh77@gmail.com>
2024-06-11 09:16:30 +0900

    test

--- artWork/urls.py
+++ artWork/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views  # artWork의 views를 import
 
 urlpatterns = [
-    path('', views.index, name='index'),  # 예를 들어, index view로 연결
+    path('artWork/', views.index, name='index'),  # 예를 들어, index view로 연결
 ]

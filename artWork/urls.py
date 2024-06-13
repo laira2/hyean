22eab22e6e84a7104f526f2a2e7e6f19990411e0
haneshi <wackymob98@naver.com>
2024-06-14 06:35:55 +0900

    수정 템플릿 보완 무한스크롤 뷰 수정

--- artWork/urls.py
+++ artWork/urls.py
@@ -5,6 +5,7 @@ from .detail_page import detail_view
 urlpatterns = [
     # path('', views.index, name='index'),  # 예를 들어, index view로 연결
     path('', views.openapi_view, name='index'),
+    # path('', views.infiniteView, name='index'), infiniteView로 메인페이지 전체 띄우기 작업을 하기 위함 준비
     # path('<str:artNm>/', views.search, name='search_query'),
     # path('search/', views.search, name='search_query'),
     path('search/', views.search, name='search'),
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -57,14 +57,12 @@ $(function(){
         speed : 700,
         autoplay : true,            // 자동 스크롤 사용 여부
         autoplaySpeed : 2000,         // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
-        pauseOnHover : true,        // 슬라이드 이동    시 마우스 호버하면 슬라이더 멈추게 설정
+        pauseOnHover : false,        // 슬라이드 이동    시 마우스 호버하면 슬라이더 멈추게 설정
         vertical : false,        // 세로 방향 슬라이드 옵션
         prevArrow : "<button type='button' class='slick-prev'>Previous</button>",        // 이전 화살표 모양 설정
         nextArrow : "<button type='button' class='slick-next'>Next</button>",        // 다음 화살표 모양 설정
         dotsClass : "slick-dots",     //아래 나오는 페이지네이션(점) css class 지정
         draggable : true,     //드래그 가능 여부
-
-
     });
   })
 

d0c063b219a2d550926cf76c496ef6c92ddb2cb2
JeongChangYeol <devcy1029@gmail.com>
2024-06-14 17:35:52 +0900

    infiniteView Complete

--- artWork/urls.py
+++ artWork/urls.py
@@ -8,9 +8,10 @@ urlpatterns = [
     # path('', views.infiniteView, name='index'), infiniteView로 메인페이지 전체 띄우기 작업을 하기 위함 준비
     # path('<str:artNm>/', views.search, name='search_query'),
     # path('search/', views.search, name='search_query'),
+    path('infinite-view/', views.infiniteView, name='infinite-view'),
+    path('search/', views.search, name='search'),
     path('detail/<str:artCd>/', detail_page.detail_view, name='detail'),
-    path('serch/', views.search, name='search'),
     # path('artwork-api/', views.artwork_api, name='artwork_api'),# 예를 들어, index view로 연결
     # path('detail/<str:art_name>/', detail_view, name='detail'),
-    path('infinite-view/', views.infiniteView, name='infinite-view'),
+
 ]

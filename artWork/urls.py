10f98277cb87c992087a30d575e90cc91bc9d21f
JeongChangYeol <devcy1029@gmail.com>
2024-06-14 09:34:46 +0900

    디테ì작품 디테일 작업 완료

--- artWork/urls.py
+++ artWork/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
-from .detail_page import detail_view
+from . import detail_page
 
 urlpatterns = [
     # path('', views.index, name='index'),  # 예를 들어, index view로 연결
     path('', views.openapi_view, name='index'),
     # path('<str:artNm>/', views.search, name='search_query'),
     # path('search/', views.search, name='search_query'),
+    path('detail/<str:artCd>/', detail_page.detail_view, name='detail'),
     path('search/', views.search, name='search'),
-    path('detail/<str:art_name>/', detail_view, name='detail'),
     # path('artwork-api/', views.artwork_api, name='artwork_api'),# 예를 들어, index view로 연결
 ]

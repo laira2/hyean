e9fa08a7936573473a985f842408fbc03fecf1ff
laira2 <76409433+laira2@users.noreply.github.com>
2024-06-14 09:40:22 +0900

    Delete shop directory
    현재프로젝트에서는 필요하지 않은 app

--- shop/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    # 추가 필드들...
-
-    def __str__(self):
-        return self.name

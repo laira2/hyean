f9727c0ddcc3b061b111fb92d90dd6697da2f455
laira2 <laira241@gmail.com>
2024-06-17 12:39:36 +0900

    cart db 생성

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Cart(models.Model):
+    user= models.OneToOneField(User,on_delete=models.CASCADE)
+class CartAddedItem(models.Model):
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE)
+    artCd = models.CharField(max_length=30)
+    art_name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.CharField(max_length=50)

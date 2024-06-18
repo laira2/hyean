72ce94cdbf2626cf79aae98707574829cba2780c
aiden_Lee <isyouh77@gmail.com>
2024-06-18 13:00:14 +0900

    DB_update

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,8 @@
 # orders/urls.py
 from django.urls import path
-from .views import order_view
 from .views import order_page
 
 urlpatterns = [
-    path('', order_view, name='order'),
+    path('', order_page, name='order'),
     path('order_page', order_page, name='order_page'),
 ]

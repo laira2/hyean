3bbaa5bf04173815864d21d3bf6fd744745e6cf3
aiden_Lee <isyouh77@gmail.com>
2024-06-19 14:50:21 +0900

    models.py수정

--- orders/models.py
+++ orders/models.py
@@ -26,3 +26,5 @@ class Ordered(models.Model):
     payment_status = models.CharField(max_length=20)
     paid_amount = models.DecimalField(max_digits=10, decimal_places=2)
     paid_at = models.DateTimeField(auto_now_add=True)
+
+

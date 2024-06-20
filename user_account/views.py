970c477bbff92ab6520139b5921b718005fcadef
laira2 <laira241@gmail.com>
2024-06-20 13:56:00 +0900

    주문 내역 ãtable 생성

--- user_account/views.py
+++ user_account/views.py
@@ -69,26 +69,44 @@ def signup(request): #회원가입
     return render(request, 'signup.html',{"signup_form":signup_form})
 @login_required
 def account(request):
-    user_info= request.user
+    user = request.user
+    orders = Order.objects.filter(user=request.user).order_by('-created_at')
+    ordered_list=[]
+    if orders:
 
-    print(order_items)
-    return render(request, 'account.html', {'user':user_info})
+        for order in orders:
+            ordered_value  = Ordered.objects.filter(order=order)
+            ordered_list.extend(ordered_value)
+        print(ordered_list)
+        return render(request, 'account.html',{'user':user, 'ordered_list':ordered_list})
+    else:
+        message = "주문 내역이 없습니다."
+        return render(request, 'account.html', {'message': message})
 
 def order_list(request):
     user= request.user
-    orders = Order.objects.filter(user=user)
-    # 주문 내역을 담을 리스트 초기화
-    order_items = []
+    ordered = Ordered.objects.filter(user=user)
+    print(ordered)
+    if orders :
 
-    # 각 주문(Order)에 대한 Ordered 객체와 OrderItem 객체 가져오기
-    for order in orders:
-        ordered = Ordered.objects.filter(order=order).first()  # 각 주문에 대한 첫 번째 Ordered 객체 가져오기
-        if ordered:
-            # 각 Ordered 객체에 연결된 OrderItem 객체들 가져오기
-            items = OrderItem.objects.filter(order=ordered.order)
-            order_items.extend(items)
+        # 주문 내역을 담을 리스트 초기화
+        order_items = []
+        # 각 주문(Order)에 대한 Ordered 객체와 OrderItem 객체 가져오기
+        for order in orders:
+            ordered = Ordered.objects.filter(order=order).first()  # 각 주문에 대한 첫 번째 Ordered 객체 가져오기
+            if ordered:
+                # 각 Ordered 객체에 연결된 OrderItem 객체들 가져오기
+                items = OrderItem.objects.filter(order=ordered.order)
+                order_items.extend(items)
+                return render(request, 'account.html',
+                              {'user': user, 'orders': orders, 'order_items': order_items})
+            else:
+                message ="주문 내역이 없습니다."
+                return render(request, 'account.html',{'message':message})
+    else:
+        message = "주문 내역이 없습니다."
+        return render(request, 'account.html', {'message': message})
 
-    return render(request, 'account.html', {'user': user_info, 'orders': orders, 'order_items': order_items})
 
 @login_required
 def delete_account(request):
